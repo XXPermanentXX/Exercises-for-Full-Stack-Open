15d5747c4c0ed47872929e20ab04fd3603f6f10f
yizhangt <92624196+XXPermanentXX@users.noreply.github.com>
2024-06-10 15:14:36 +0200

    part1_1.12-1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const initialAnecdoteData = anecdotes.map((anecdote) => ({
+    content: anecdote,
+    votes: 0,
+  }));
+
+  const [selected, setSelected] = useState(0);
+  const [mostVotes, setMostVotes] = useState(0);
+  const [anecdoteData, setAnecdoteData] = useState(initialAnecdoteData);
+
+  const handleNextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdoteData.length));
+  };
+
+  const handleVote = () => {
+    const newAnecdoteData = [...anecdoteData];
+    newAnecdoteData[selected].votes += 1;
+    setAnecdoteData(
+      newAnecdoteData.sort((a, b) => {
+        b.votes - a.votes;
+      }),
+    );
+    setMostVotes(findMostVotes());
+  };
+
+  const findMostVotes = () => {
+    let max = 0;
+    let maxIndex = 0;
+    anecdoteData.map((anecdote, index) => {
+      if (anecdote.votes > max) {
+        max = anecdote.votes;
+        maxIndex = index;
+      }
+    });
+    return maxIndex;
+  };
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdoteData[selected].content}</div>
+      <div>has {anecdoteData[selected].votes} votes</div>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNextAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdoteData[mostVotes].content}</div>
+      <div>has {anecdoteData[mostVotes].votes} votes</div>
+    </div>
+  );
+};
+
+export default App;
